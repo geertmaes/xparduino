0f4a76d0294f4dff035b1c915e64579c42828781
Maes Geert <Geert.Maes@cegeka.com>
2017-05-19 21:51:34 +0200

    testing serialport configuration

--- XPArduino_Application/src/main/java/Application.java
+++ XPArduino_Application/src/main/java/Application.java
@@ -1,5 +1,19 @@
-public class Application {
-    public static void main(String[] args) {
+import jssc.SerialPort;
+import jssc.SerialPortException;
 
+
+public class Application {
+
+    public static final String ARDUINO_SERIAL_PORT = "COM3";
+
+    public static void main(String[] args) {
+        SerialPort serialPort = new SerialPort(ARDUINO_SERIAL_PORT);
+        try {
+            serialPort.openPort();
+            serialPort.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+
+        } catch (SerialPortException e) {
+            System.err.println(e);
+        }
     }
 }
