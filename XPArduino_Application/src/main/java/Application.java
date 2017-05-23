2b656b05ee5213362bfe27b486808b57f5ec53b1
Maes Geert <Geert.Maes@cegeka.com>
2017-05-23 19:39:54 +0200

    sandbox2

--- XPArduino_Application/src/main/java/Application.java
+++ XPArduino_Application/src/main/java/Application.java
@@ -1,19 +1,56 @@
 import jssc.SerialPort;
 import jssc.SerialPortException;
+import jssc.SerialPortList;
+
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 public class Application {
 
-    public static final String ARDUINO_SERIAL_PORT = "COM3";
 
-    public static void main(String[] args) {
-        SerialPort serialPort = new SerialPort(ARDUINO_SERIAL_PORT);
-        try {
-            serialPort.openPort();
-            serialPort.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+    public static void main(String[] args) throws Exception {
+        new Application().run();
+    }
 
-        } catch (SerialPortException e) {
-            System.err.println(e);
-        }
+    public Application(){
+
+    }
+
+    public void run() throws Exception{
+        Scanner scanner = new Scanner(System.in);
+
+        List<SerialPort> availablePorts = getAvailablePorts();
+        System.out.println("Available ports: \n" + toString(availablePorts));
+        SerialPort selectedPort = availablePorts.get(scanner.nextInt());
+
+        selectedPort.openPort();
+        selectedPort.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+
+        InputStream input = selectedPort.get();
+        output = serialPort.getOutputStream();
+        writeData(0, 0);
+
+        successful = true;
+        return successful;
+
+    }
+
+    private String toString(List<SerialPort> ports) {
+        return IntStream.range(0, ports.size())
+                .mapToObj(index -> index + ") " + ports.get(index).getPortName())
+                .collect(Collectors.joining(","));
+    }
+
+    private List<SerialPort> getAvailablePorts() {
+        return Arrays.stream(SerialPortList.getPortNames())
+                .map(SerialPort::new)
+                .collect(Collectors.toList());
     }
 }
