40e53dc40549d7c218ac60c14234c9cfb3899825
kevinb <kevinbinon@gmail.com>
2017-05-30 18:18:36 +0200

    led toggle-ing adhv bulbke

--- XPArduino_GUI/src/app/app.component.ts
+++ XPArduino_GUI/src/app/app.component.ts
@@ -1,24 +1,41 @@
 import {Component, OnInit} from '@angular/core';
 import {ComPortService} from "./service/com-port.service";
 import {LedService} from "./service/led.service";
+import {Response} from "@angular/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'app works!';
   ledStatus = false;
-  comPorts = ["COM1", "COM2"];
+  comPorts;
+  comPort;
+  lightBulbSrc = "assets/lightbulb-off.png";
 
   constructor(private comPortService: ComPortService, private ledService: LedService) {
   }
 
 
   ngOnInit(): void {
-    this.comPortService.getComPorts()
+    this.comPortService.getComPorts().subscribe(data =>
+      this.comPorts = data);
   }
 
+  openPort() {
+    this.comPortService.openPort(this.comPort);
+  }
+
+  toggleLed() {
+    this.ledStatus = !this.ledStatus;
+    this.ledService.toggleLed(this.ledStatus);
+    if (this.ledStatus) {
+      this.lightBulbSrc = "assets/lightbulb-on.png";
+    } else {
+      this.lightBulbSrc = "assets/lightbulb-off.png";
+    }
+  }
 
 }
