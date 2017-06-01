4375ce046b927f12ca4d53e1508cebf6a21cca95
kevinb <kevinbinon@gmail.com>
2017-06-01 21:06:19 +0200

    show spinner when port is opening

--- XPArduino_GUI/src/app/material.ts
+++ XPArduino_GUI/src/app/material.ts
@@ -1,8 +1,11 @@
-import {MdButtonModule, MdGridListModule, MdSlideToggleModule, MdSelectModule} from "@angular/material";
+import {
+  MdButtonModule, MdGridListModule, MdSlideToggleModule, MdSelectModule, MdSpinner,
+  MdProgressSpinnerModule
+} from "@angular/material";
 import {NgModule} from "@angular/core";
 @NgModule({
-  imports: [MdButtonModule, MdSlideToggleModule, MdGridListModule, MdSelectModule],
-  exports: [MdButtonModule, MdSlideToggleModule, MdGridListModule, MdSelectModule]
+  imports: [MdButtonModule, MdSlideToggleModule, MdGridListModule, MdSelectModule, MdProgressSpinnerModule],
+  exports: [MdButtonModule, MdSlideToggleModule, MdGridListModule, MdSelectModule, MdProgressSpinnerModule]
 })
 
 export class MaterialGroupModule {
