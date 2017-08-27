c860c5fc6d6dce634135d2b1d5bd798f9e349431
David Op de Beeck <davidopdebeeck@hotmail.com>
2017-08-27 21:27:08 +0200

    Handle new events every 100 milliseconds

--- xparduino-library/src/main/java/com/cegeka/xparduino/queue/ArduinoQueueReceiverHandler.java
+++ xparduino-library/src/main/java/com/cegeka/xparduino/queue/ArduinoQueueReceiverHandler.java
@@ -4,33 +4,36 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public class ArduinoQueueReceiverHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ArduinoQueueReceiverHandler.class);
+    private static final int LISTEN_PERIOD = 100;
+    private static final int LISTEN_DELAY = 1000;
 
     private final ArduinoQueue queue;
     private final List<ArduinoQueueReceiver> queueReceivers;
-    private final ExecutorService executorService;
+    private final ScheduledExecutorService executorService;
 
     public ArduinoQueueReceiverHandler(ArduinoQueue queue, List<ArduinoQueueReceiver> queueReceivers) {
         this.queue = queue;
         this.queueReceivers = queueReceivers;
-        this.executorService = Executors.newSingleThreadExecutor();
+        this.executorService = Executors.newScheduledThreadPool(4);
     }
 
-    @SuppressWarnings("InfiniteLoopStatement")
     public void listenOnQueueChanges() {
-        executorService.submit(() -> {
-            while (true) {
-                if (queue.hasNext()) {
-                    String message = queue.next();
-                    LOGGER.info("Received message ({}) from queue", message);
-                    queueReceivers.forEach(receiver -> receiver.onMessage(message));
-                }
-            }
-        });
+        executorService.scheduleAtFixedRate(this::handleQueueEvent, LISTEN_DELAY, LISTEN_PERIOD, MILLISECONDS);
+    }
+
+    private void handleQueueEvent() {
+        if (queue.hasNext()) {
+            String message = queue.next();
+            LOGGER.info("Received message ({}) from queue", message);
+            queueReceivers.forEach(receiver -> receiver.onMessage(message));
+        }
     }
 }
